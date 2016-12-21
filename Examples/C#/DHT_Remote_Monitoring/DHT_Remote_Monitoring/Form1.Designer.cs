af2b53a5b94f33e2ab46eebadca73e40ee8c9e46
Invent Victor <victorshoaga@gmail.com>
2016-12-21 10:55:59 +0100

    add examples

---
+++ Examples/C#/DHT_Remote_Monitoring/DHT_Remote_Monitoring/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace DHT_Remote_Monitoring
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ip_address_input = new System.Windows.Forms.TextBox();
+            this.data_box = new System.Windows.Forms.TextBox();
+            this.monitor_button = new System.Windows.Forms.Button();
+            this.continuously_monitor = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.time_interval = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.temperature_textBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.humidity_textBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // ip_address_input
+            // 
+            this.ip_address_input.Location = new System.Drawing.Point(431, 55);
+            this.ip_address_input.Name = "ip_address_input";
+            this.ip_address_input.Size = new System.Drawing.Size(100, 20);
+            this.ip_address_input.TabIndex = 0;
+            // 
+            // data_box
+            // 
+            this.data_box.Location = new System.Drawing.Point(12, 55);
+            this.data_box.Multiline = true;
+            this.data_box.Name = "data_box";
+            this.data_box.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.data_box.Size = new System.Drawing.Size(413, 271);
+            this.data_box.TabIndex = 1;
+            this.data_box.TextChanged += new System.EventHandler(this.data_box_TextChanged);
+            // 
+            // monitor_button
+            // 
+            this.monitor_button.Location = new System.Drawing.Point(431, 93);
+            this.monitor_button.Name = "monitor_button";
+            this.monitor_button.Size = new System.Drawing.Size(100, 38);
+            this.monitor_button.TabIndex = 2;
+            this.monitor_button.Text = "Monitor";
+            this.monitor_button.UseVisualStyleBackColor = true;
+            this.monitor_button.Click += new System.EventHandler(this.monitor_button_Click);
+            // 
+            // continuously_monitor
+            // 
+            this.continuously_monitor.AutoSize = true;
+            this.continuously_monitor.Location = new System.Drawing.Point(451, 137);
+            this.continuously_monitor.Name = "continuously_monitor";
+            this.continuously_monitor.Size = new System.Drawing.Size(122, 17);
+            this.continuously_monitor.TabIndex = 4;
+            this.continuously_monitor.Text = "continuously monitor";
+            this.continuously_monitor.UseVisualStyleBackColor = true;
+            this.continuously_monitor.CheckedChanged += new System.EventHandler(this.continuously_monitor_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(451, 160);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(77, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "save to file";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = ".";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(428, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter IP Address";
+            // 
+            // time_interval
+            // 
+            this.time_interval.Location = new System.Drawing.Point(431, 199);
+            this.time_interval.Name = "time_interval";
+            this.time_interval.ReadOnly = true;
+            this.time_interval.Size = new System.Drawing.Size(25, 20);
+            this.time_interval.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(431, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Time Interval";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(534, 210);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(457, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "secs";
+            // 
+            // temperature_textBox
+            // 
+            this.temperature_textBox.Location = new System.Drawing.Point(431, 305);
+            this.temperature_textBox.Name = "temperature_textBox";
+            this.temperature_textBox.Size = new System.Drawing.Size(100, 20);
+            this.temperature_textBox.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(431, 250);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Humidity";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(431, 289);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Temperature";
+            // 
+            // humidity_textBox
+            // 
+            this.humidity_textBox.Location = new System.Drawing.Point(431, 266);
+            this.humidity_textBox.Name = "humidity_textBox";
+            this.humidity_textBox.Size = new System.Drawing.Size(100, 20);
+            this.humidity_textBox.TabIndex = 12;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(428, 234);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(94, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Set Upper Limit";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(534, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "%";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(532, 306);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(22, 17);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "*C";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(529, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 28);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(498, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 28);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(581, 337);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.humidity_textBox);
+            this.Controls.Add(this.temperature_textBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.time_interval);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.continuously_monitor);
+            this.Controls.Add(this.monitor_button);
+            this.Controls.Add(this.data_box);
+            this.Controls.Add(this.ip_address_input);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Remote Monitoring System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ip_address_input;
+        private System.Windows.Forms.TextBox data_box;
+        private System.Windows.Forms.Button monitor_button;
+        private System.Windows.Forms.CheckBox continuously_monitor;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox time_interval;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox temperature_textBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox humidity_textBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Examples/C#/DHT_Remote_Monitoring/DHT_Remote_Monitoring/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DHT_Remote_Monitoring
+{
+    public partial class Form1 : Form
+    {
+        bool cont_monitor = false;
+        int t_plus = 1;
+        Timer t;
+        string filePath, file_text = "";
+        float iHum, iTemp;
+        string sHum, sTemp;
+        char[] f;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            time_interval.Text = t_plus.ToString().Trim();
+            
+        }
+
+        private bool IsValidIP(string addr)
+        {
+            string pattern = @"^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$";
+            Regex check = new Regex(pattern);
+            bool valid = false;
+            if (String.IsNullOrEmpty(addr))
+            {
+                valid = false;
+            }
+            else
+            {
+                valid = check.IsMatch(addr, 0);
+            }
+            return valid;
+        }
+
+        private bool IsValidHumidity(string humidity) 
+        {
+            string pattern = @"^(?:\d{1,2})?(?:\.\d{1,2})?$";
+            Regex check = new Regex(pattern);
+            bool valid;
+            if (String.IsNullOrEmpty(humidity))
+            {
+                valid = false;
+            }
+            else 
+            { 
+                valid = check.IsMatch(humidity, 0); 
+            }
+
+            return valid;
+        }
+
+        private bool IsValidTemperature(string temperature)
+        {
+            string pattern = @"^(?:\d{1,2})?(?:\.\d{1,2})?$";
+            Regex check = new Regex(pattern);
+            bool valid;
+            if (String.IsNullOrEmpty(temperature))
+            {
+                valid = false;
+            }
+            else
+            {
+                valid = check.IsMatch(temperature, 0);
+            }
+
+            return valid;
+        }
+
+        private void monitor_button_Click(object sender, EventArgs e)
+        {
+            string ip_addr = ip_address_input.Text.ToString().Trim();
+            string humidity = humidity_textBox.Text.ToString().Trim();
+            string temperature = temperature_textBox.Text.ToString().Trim();
+            if (!IsValidIP(ip_addr))
+            {
+
+                MessageBox.Show("Please input correct IP Address", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!IsValidHumidity(humidity)) 
+            {
+                MessageBox.Show("Please input upper limit of humidity", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!IsValidTemperature(temperature))
+            {
+                MessageBox.Show("Please input upper limit of temperature", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            string response = requestSensorData(ip_addr);
+            f = response.ToArray();
+            sHum = "";
+            sTemp = "";
+            for (int i = 0; i < response.Length; i++)
+            {
+                if (9 <= i && i <= 12)
+                {
+                    sHum += f[i];
+                }
+
+                if (28 <= i && i <= 31)
+                {
+                    sTemp += f[i];
+                }
+
+            }
+            
+            Debug.WriteLine(sHum);
+            Debug.WriteLine(sTemp);
+            float.TryParse(sHum, out iHum);
+            float.TryParse(sTemp, out iTemp);
+
+            Debug.WriteLine(iHum);
+            Debug.WriteLine(iTemp);
+           
+            if (iHum > float.Parse(humidity) || iTemp > float.Parse(temperature))
+            {
+                makeBuzzerSound(ip_addr, "1");
+                Debug.WriteLine("ok");
+            }
+            else
+            {
+                makeBuzzerSound(ip_addr, "0");
+                Debug.WriteLine("oj");
+            }
+            Debug.WriteLine(date);
+            Debug.WriteLine(response);
+            string text_to_write = response + "  " + date + Environment.NewLine;
+            data_box.Text += text_to_write;
+            file_text += text_to_write;
+            if (checkBox2.Checked && !String.IsNullOrEmpty(filePath)) 
+            {
+                File.WriteAllText(filePath, file_text);
+            }
+        }
+
+        private string requestSensorData(string ip_addr) 
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://"+ip_addr+"/invent_one?action=monitor_sensor&sensor=DHT11");
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            return responseString.ToString().Trim();
+        }
+
+        private string makeBuzzerSound(string ip_addr, string data) 
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://" + ip_addr + "/invent_one?action=control_pin_digital&pin=8&state="+data);
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            return responseString.ToString().Trim();
+        }
+
+        private void continuously_monitor_CheckedChanged(object sender, EventArgs e)
+        {
+            if (continuously_monitor.Checked)
+            {
+                cont_monitor = true;
+                t = new Timer();
+                t.Interval += Int32.Parse(time_interval.Text.ToString().Trim()) * 1000;
+                t.Tick += new EventHandler(DoThisWhileContinuousChecked);
+                t.Start();
+            }
+            else 
+            { 
+                cont_monitor = false;
+                t.Stop();
+                Debug.WriteLine("ok");
+            }
+        }
+
+        private void DoThisWhileContinuousChecked(object sender, EventArgs e) 
+        {
+            monitor_button.PerformClick();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t_plus += 1;
+            time_interval.Text = t_plus.ToString().Trim();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (t_plus != 1) 
+            {
+                t_plus -= 1;
+                time_interval.Text = t_plus.ToString().Trim();
+            }
+        }
+
+        private void data_box_TextChanged(object sender, EventArgs e)
+        {
+            data_box.SelectionStart = data_box.Text.Length;
+            data_box.ScrollToCaret();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked) 
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.InitialDirectory = @"C:\";
+                openFileDialog1.Title = "Browse Text File";
+                openFileDialog1.CheckFileExists = true;
+                openFileDialog1.CheckPathExists = true;
+                openFileDialog1.DefaultExt = "txt";
+                openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.RestoreDirectory = true;
+                openFileDialog1.ReadOnlyChecked = false;
+                openFileDialog1.ShowReadOnly = true;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog1.FileName;
+                }
+            }
+            
+        }
+
+    }
+}
